eaed72b06d70ede012e13ab07df3f1c50b9718ea
sayali1227 <51477889+sayali1227@users.noreply.github.com>
2022-09-16 17:49:42 +0530

    Delete ParentComponent.js

--- ParentComponent.js
+++
@@ -1,9 +0,0 @@
-import InputBox from "./InputBox";
-function ParentComponent(){
-    return(
-        <div>
-        <InputBox></InputBox>
-        </div>
-    );
-}
-export default ParentComponent;

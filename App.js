5c2aabdc692bf18e48f1324fe4fba05e088f5bb6
sayali1227 <51477889+sayali1227@users.noreply.github.com>
2022-09-24 18:52:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AddMovie from './components/AddForm';
+import GetAPI from './components/GetAPI';
+
+function App() {
+  return (
+    <div className="App">
+      <AddMovie/>
+      <GetAPI/>
+    </div>
+  );
+}
+
+export default App;
